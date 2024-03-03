9c067611f2009ac3a794d4f4aa3993f85b884028
ublyudok <ivan.karankin.01@bk.ru>
2024-03-03 22:38:28 +0300

    second

--- js/Profile.js
+++ js/Profile.js
@@ -1,6 +1,11 @@
 var profileBtn = document.getElementById('profile_btn');
 var dropDownMenu = document.getElementById('mini_dropdown_menu');
 var arrowDropDown = document.getElementById('arrow_profile');
+var emailInput = document.getElementById('emailInput');
+var editBtn = document.getElementById('editBtn');
+var switchContainer = document.getElementById('switchContainer');
+var audioContainer = document.getElementById('audioContainer');
+var postContainer = document.getElementById('ShowPost');
 
 profileBtn.onclick = function() {
     dropDownMenu.style.display = 'block';
@@ -18,3 +23,20 @@ document.addEventListener('click', (e) => {
         arrowDropDown.classList.add('arrow_icon_down');
     }
 })
+
+editBtn.onclick = function() {
+    emailInput.removeAttribute('disabled');
+    editBtn.style.display = 'none';
+}
+
+function toggledSwitch() {
+    switchContainer.classList.toggle('switch_active');
+}
+
+function audioSwitcher() {
+    audioContainer.classList.toggle('switch_active');
+}
+
+function ShowPost() {
+    postContainer.classList.toggle('switch_active');
+}
